52eba985011a7e3921391543a113506c68298865
Hyerin-oh <ohhyerin.oh@gmail.com>
2022-02-12 16:39:56 +0900

    refactor: list로 된 config를 받도록 수정

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import logging
 import random
 import re
 from datetime import datetime, timedelta
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, Tuple
 
 import requests
 from slack_sdk import WebClient
@@ -36,17 +36,18 @@ def make_person_dict(notion_api_token: str, notion_version: str) -> Dict[str, st
     return user_dict
 
 
-def retrieve_databases(cfg: Dict[str, Any]) -> Dict[str, Any]:
+def retrieve_databases(base_cfg: Dict[str, Any], team_name: str) -> Dict[str, Any]:
     """
     원하는 조건(체크인 문서)에 맞춰 필터링 된 Database 중 가장 최근에 생성된 page 1개에 대한 정보를 가져옵니다.
 
-    :param cfg: 실행에 필요한 여러 정보가 담긴 json
+    :param base_cfg: 팀과는 상관없이 실행에 필요한 토큰 등의 정보들이 담긴 config
+    :param team_name: 필터링에 사용할 팀 명
     :return: json 형식의 최근 작성된 체크인 문서에 대한 정보
     """
-    url = f"https://api.notion.com/v1/databases/{cfg['database_id']}/query"
+    url = f"https://api.notion.com/v1/databases/{base_cfg['database_id']}/query"
     filter = {
         "and": [
-            {"property": "Tags", "multi_select": {"contains": cfg["team_name"]}},
+            {"property": "Tags", "multi_select": {"contains": team_name}},
             {"property": "Tags", "multi_select": {"contains": "OKR"}},
             {"property": "회의 유형", "multi_select": {"contains": "Check-in"}},
         ]
@@ -55,8 +56,8 @@ def retrieve_databases(cfg: Dict[str, Any]) -> Dict[str, Any]:
     response = requests.post(
         url,
         headers={
-            "Authorization": f"Bearer {cfg['notion_api_token']}",
-            "Notion-Version": cfg["notion_version"],
+            "Authorization": f"Bearer {base_cfg['notion_api_token']}",
+            "Notion-Version": base_cfg["notion_version"],
         },
         json={"filter": filter, "sorts": sort, "page_size": 1},
     )
@@ -64,33 +65,36 @@ def retrieve_databases(cfg: Dict[str, Any]) -> Dict[str, Any]:
     return response.json()
 
 
-def check_already_made(cfg) -> Tuple[bool, Any, Any, Any, Any]:
+def check_already_made(base_cfg: Dict[str, Any], team_cfg: Dict[str, Any]) -> Tuple[bool, Any, Any, Any, Any]:
     """
     retrieve_databases 함수를 통해 가장 최근에 만들어진 문서에 대한 정보를 가져옵니다.
     이후 만들어진 지 1주일이 되지 않았다면 False와 최근 만들어진 문서의 url를 반환합닏다.
     만약 1주일이 되었다면 True와 가장 마지막으로 작성된 체크인 날짜, 인덱스, 분기를 반환합니다.
 
-    :param cfg: 실행에 필요한 여러 정보가 담긴 json
+    :param base_cfg: 팀과는 상관없이 실행에 필요한 토큰 등의 정보들이 담긴 config
+    :param team_cfg: 팀 별 체크인 문서 생성에 필요한 정보들이 담긴 config
     :return: Tuple(작성 여부, 가장 마지막으로 작성된 체크인 날짜, 인덱스, 분기, 최신 문서 url)
     """
-    data = retrieve_databases(cfg)["results"][0]
-    latest_created_time = datetime.strptime(data["created_time"], "%Y-%m-%dT%H:%M:%S.%fZ")
+    latest_checkin_info = retrieve_databases(base_cfg, team_cfg["team_name"])
+    latest_checkin_data = latest_checkin_info["results"][0]
+    latest_created_time = datetime.strptime(latest_checkin_data["created_time"], "%Y-%m-%dT%H:%M:%S.%fZ")
     created_time_diff = (datetime.today() - latest_created_time).days
 
     if created_time_diff < 7:
         # 최근 체크인 문서가 작성된 지 일주일도 되지 않았다는 것은 사람이 직접 만들었다는 뜻으로 작성하지 않음.
-        latest_url = data["url"]
+        latest_url = latest_checkin_data["url"]
         return True, None, None, None, latest_url
 
-    latest_title = data["properties"]["제목"]["title"][0]["plain_text"]
-    latest_quater = data["properties"]["Quarter"]["select"]["name"]
+    latest_title = latest_checkin_data["properties"]["제목"]["title"][0]["plain_text"]
+    latest_quater = latest_checkin_data["properties"]["Quarter"]["select"]["name"]
     latest_checkin_day = re.search("[0-9]{6}", latest_title).group()
     latest_index = int(re.search("#([0-9]{1,})", latest_title).group(1))
     return False, latest_checkin_day, latest_index, latest_quater, None
 
 
 def create_pages(
-    cfg: Dict[str, Any],
+    base_cfg: Dict[str, Any],
+    team_cfg: Dict[str, Any],
     latest_checkin_day: str,
     latest_index: int,
     latest_quater: str,
@@ -100,37 +104,38 @@ def create_pages(
     체크인 문서의 Quarter는 이전 체크인 문서의 Quarter를 , 제목의 #n 은 이전 체크인 문서의 인덱스+1 이 됩니다.
     새로 생성한 체크인 문서의 url을 리턴해줍니다.
 
-    :param cfg: 실행에 필요한 여러 정보가 담긴 json
+    :param base_cfg: 팀과는 상관없이 실행에 필요한 토큰 등의 정보들이 담긴 config
+    :param team_cfg: 팀 별 체크인 문서 생성에 필요한 정보들이 담긴 config
     :param latest_checkin_day: 가장 최근 작성된 체크인 문서의 날짜
     :param latest_index: 가장 최근 작성된 체크인 문서의 인덱스
     :param latest_quater: 가장 최근 작성된 체크인 문서의 분기 정보
     :return: 새로 작성된 페이지의 url
     """
     # 주최자는 회의록 작성자가 될 수 없습니다.
-    user_name2id = make_person_dict(cfg["notion_api_token"], cfg["notion_version"])
-    writer_list = [user_name2id[name] for name in cfg["people_list"]]
-    writer_list.remove(user_name2id[cfg["host"]])
+    user_name2id = make_person_dict(base_cfg["notion_api_token"], base_cfg["notion_version"])
+    writer_list = [user_name2id[name] for name in team_cfg["people_list"]]
+    writer_list.remove(user_name2id[team_cfg["host"]])
 
     check_in_day = datetime.strptime(latest_checkin_day, "%y%m%d") + timedelta(weeks=1)
-    title = f"[{check_in_day.strftime('%y%m%d')}] {cfg['base_title']}{latest_index + 1}"
+    title = f"[{check_in_day.strftime('%y%m%d')}] {team_cfg['base_title']}{latest_index + 1}"
     payload = {
-        "parent": {"database_id": cfg["database_id"]},
+        "parent": {"database_id": base_cfg["database_id"]},
         "properties": {
             "제목": {"title": [{"type": "text", "text": {"content": title}}]},
             "날짜": {"date": {"start": check_in_day.strftime(("%Y-%m-%d"))}},
             "Quarter": {"select": {"name": latest_quater}},
-            "주최자": {"people": [{"object": "user", "id": user_name2id[cfg["host"]]}]},
-            "참석자": {"people": [{"object": "user", "id": user_name2id[id]} for id in cfg["people_list"]]},
+            "주최자": {"people": [{"object": "user", "id": user_name2id[team_cfg["host"]]}]},
+            "참석자": {"people": [{"object": "user", "id": user_name2id[id]} for id in team_cfg["people_list"]]},
             "회의록 작성자": {"people": [{"object": "user", "id": random.choice(writer_list)}]},
-            "Tags": {"multi_select": [{"name": "OKR"}, {"name": cfg["team_name"]}]},
+            "Tags": {"multi_select": [{"name": "OKR"}, {"name": team_cfg["team_name"]}]},
             "회의 유형": {"multi_select": [{"name": "Check-in"}]},
         },
     }
     response = requests.post(
         "https://api.notion.com/v1/pages",
         headers={
-            "Authorization": f"Bearer {cfg['notion_api_token']}",
-            "Notion-Version": cfg["notion_version"],
+            "Authorization": f"Bearer {base_cfg['notion_api_token']}",
+            "Notion-Version": base_cfg["notion_version"],
         },
         json=payload,
     )
@@ -149,40 +154,56 @@ def main(args: argparse.Namespace):
     """
     with open(args.config_path) as f:
         cfg = json.load(f)
-    necessary_key = [
-        "slack_bot_token",
-        "notion_api_token",
-        "database_id",
-        "notion_version",
-        "channel_id",
-        "team_name",
-        "base_title",
-        "host",
-        "people_list",
-    ]
 
-    for key in necessary_key:
-        assert key in cfg.keys(), f"config에는 {key}가 필요합니다. 추가해서 다시 시도해주세요."
+    base_cfg = cfg["base"]
+    team_cfg_list = cfg["team"]
 
-    client = WebClient(token=cfg["slack_bot_token"])
+    # config 내 필요한 정보들이 들어있는 지 확인.
+    necessary_key = {
+        "base": [
+            "slack_bot_token",
+            "notion_api_token",
+            "database_id",
+            "notion_version",
+        ],
+        "team": [
+            "channel_id",
+            "team_name",
+            "base_title",
+            "host",
+            "people_list",
+        ],
+    }
 
-    logging.info("[+] 일주일 내 이미 만들어진 문서가 있는 지 확인합니다.")
-    made, latest_checkin_day, latest_index, latest_quater, latest_url = check_already_made(cfg)
-    if made:
-        logging.info("[+] 일주일 내 이미 만들어진 문서가 존재하므로 체크인 문서를 새로 생성하지 않습니다.")
-        response = client.chat_postMessage(
-            channel=cfg["channel_id"], text=f"이번 주 {cfg['team_name']} 체크인 문서는 별도로 생성되지 않았습니다. \n{latest_url}"
-        )
-        response.validate()
+    for key in necessary_key["base"]:
+        assert key in base_cfg.keys(), f" base config에는 {key}가 필요합니다. 추가해서 다시 시도해주세요."
 
-    else:
-        logging.info("[+] 일주일 내 이미 만들어진 문서가 존재하지 않으므로 체크인 문서를 새로 생성합니다.")
-        new_page_url = create_pages(cfg, latest_checkin_day, latest_index, latest_quater)
-        response = client.chat_postMessage(
-            channel=cfg["channel_id"],
-            text=f"이번 주 {cfg['team_name']} 체크인 문서입니다. template을 클릭해 작성해주세요! \n {new_page_url}",
-        )
-        response.validate()
+    client = WebClient(token=base_cfg["slack_bot_token"])
+
+    for team_cfg in team_cfg_list:
+        # team_config 내 필요한 정보들이 들어있는 지 확인.
+        for key in necessary_key["team"]:
+            assert key in team_cfg.keys(), f" team config에는 {key}가 필요합니다. 추가해서 다시 시도해주세요."
+
+        logging.info("[+] 일주일 내 이미 만들어진 문서가 있는 지 확인합니다.")
+        made, latest_checkin_day, latest_index, latest_quater, latest_url = check_already_made(base_cfg, team_cfg)
+
+        if made:
+            logging.info("[+] 일주일 내 이미 만들어진 문서가 존재하므로 체크인 문서를 새로 생성하지 않습니다.")
+            response = client.chat_postMessage(
+                channel=team_cfg["channel_id"],
+                text=f"이번 주 {team_cfg['team_name']} 체크인 문서는 별도로 생성되지 않았습니다.\n{latest_url}",
+            )
+            response.validate()
+
+        else:
+            logging.info("[+] 일주일 내 이미 만들어진 문서가 존재하지 않으므로 체크인 문서를 새로 생성합니다.")
+            new_page_url = create_pages(base_cfg, team_cfg, latest_checkin_day, latest_index, latest_quater)
+            response = client.chat_postMessage(
+                channel=team_cfg["channel_id"],
+                text=f"이번 주 {team_cfg['team_name']} 체크인 문서입니다. template을 클릭해 작성해주세요!\n{new_page_url}",
+            )
+            response.validate()
 
 
 if __name__ == "__main__":
